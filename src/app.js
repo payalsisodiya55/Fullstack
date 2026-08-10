c5b8d100dee5651443bfa82e48b967f72e8e1371
payalsisodiya55 <payalsisodiya038@gmail.com>
2026-08-10 16:55:11 +0530

    connected router , controller and app.js

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Import routes
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+//    http://localhost:8000/api/v1/users/register
+
 export { app };
