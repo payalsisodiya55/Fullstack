ea4f0a5233663d6241f3608c6e5a264b12375db7
payalsisodiya55 <payalsisodiya038@gmail.com>
2026-08-10 12:37:05 +0530

    final backend setup done

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,6 +7,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Take a local file → upload it to Cloudinary → return the Cloudinary response
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
