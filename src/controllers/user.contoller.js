5750c2856aa4a05614955e1178503da1807884f1
payalsisodiya55 <payalsisodiya038@gmail.com>
2026-08-12 20:13:42 +0530

    middlware added in routes for images

--- src/controllers/user.contoller.js
+++ src/controllers/user.contoller.js
@@ -2,8 +2,9 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
-  console.log(fullName);
   console.log("email:", email);
+
+ 
 });
 
 export { registerUser };
